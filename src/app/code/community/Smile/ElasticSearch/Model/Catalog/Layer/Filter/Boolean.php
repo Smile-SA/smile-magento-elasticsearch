a3d3e8c459fe229bc82b25410ea432b46c090f2d
Nicolas Lœuillet <nicolas.loeuillet@smile.fr>
2015-07-02 16:35:07 +0200

    fix #10: boolean type not detected

--- src/app/code/community/Smile/ElasticSearch/Model/Catalog/Layer/Filter/Boolean.php
+++ src/app/code/community/Smile/ElasticSearch/Model/Catalog/Layer/Filter/Boolean.php
@@ -18,35 +18,5 @@
  */
 class Smile_ElasticSearch_Model_Catalog_Layer_Filter_Boolean extends Smile_ElasticSearch_Model_Catalog_Layer_Filter_Attribute
 {
-    /**
-     * Returns facets data of current attribute.
-     *
-     * @return array
-     */
-    protected function _getFacets()
-    {
-        $facets = parent::_getFacets();
-        $result = array();
-        foreach ($facets as $value => $count) {
-            $key = 0; // false by default
-            if ($value === 'true' || $value === 'T' || $value === '1' || $value === 1 || $value === true) {
-                $key = 1;
-            }
-            $result[$key] = $count;
-        }
 
-        return $result;
-    }
-
-    /**
-     * Checks if given filter is valid before being applied to product collection.
-     *
-     * @param string $filter Filter to be validated
-     *
-     * @return bool
-     */
-    protected function _isValidFilter($filter)
-    {
-        return $filter === '0' || $filter === '1' || false === $filter || true === $filter;
-    }
 }
