289b594ce5e58d2255dbe6dd7caa69fc136c55ca
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2015-08-06 10:16:00 +0200

    Use full categories hiearchy to match category name

--- src/app/code/community/Smile/ElasticSearch/Model/Resource/Engine/Index.php
+++ src/app/code/community/Smile/ElasticSearch/Model/Resource/Engine/Index.php
@@ -174,7 +174,7 @@ class Smile_ElasticSearch_Model_Resource_Engine_Index extends Mage_CatalogSearch
         $helper->addGroupConcatColumn($select, 'anchors', 'cat.category_id', ' ', ',', 'is_parent = 0');
         $helper->addGroupConcatColumn($select, 'positions', array('cat.category_id', 'cat.position'), ' ', '_', 'is_parent = 1');
         $helper->addGroupConcatColumn($select, 'category_name', new Zend_Db_Expr(
-             'IF(cat.category_id = 2 OR is_parent = 0, "", name.value)'), '|'
+             'IF(cat.category_id = 2, "", name.value)'), '|'
         );
 
         $select  = $helper->getQueryUsingAnalyticFunction($select);
