fbf909ee3187c0c3ca485f967a4ae386cf6a2e6c
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2015-09-16 06:39:00 +0200

    Compatibility of virtual categories with flat categories.

--- src/app/code/community/Smile/VirtualCategories/Helper/Data.php
+++ src/app/code/community/Smile/VirtualCategories/Helper/Data.php
@@ -19,4 +19,41 @@
 class Smile_VirtualCategories_Helper_Data extends Mage_Core_Helper_Data
 {
 
+    /**
+     * Local cache for loaded rules.
+     *
+     * @var array
+     */
+    protected $_categoryRulesCache = array();
+
+    /**
+     * Force the virtual rule to be loaded for a category.
+     *
+     * @param Mage_Catalog_Model_Category $category The category.
+     *
+     * @return Smile_VirtualCategories_Model_Rule
+     */
+    public function getVirtualRule($category)
+    {
+
+        $virtualRule = $category->getVirtualRule();
+
+        if (!is_object($virtualRule)) {
+            $cacheKey = $category->getId();
+
+            if ($category->getStoreId()) {
+                $cacheKey = $cacheKey . '_' . $category->getStoreId();
+            }
+
+            if (!isset($this->_categoryRulesCache[$cacheKey])) {
+                $backend = Mage::getSingleton('smile_virtualcategories/category_attributes_backend_virtual');
+                $backend->afterLoad($category);
+                $this->_categoryRulesCache[$cacheKey] = $category->getVirtualRule();
+            }
+
+            $virtualRule = $this->_categoryRulesCache[$cacheKey];
+        }
+
+        return $virtualRule;
+    }
 }
