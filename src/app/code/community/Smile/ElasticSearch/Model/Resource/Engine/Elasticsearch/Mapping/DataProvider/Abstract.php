23ee9711b126dd41e0681717419544c44fbedacf
Romain Ruaud <romain.ruaud@smile.fr>
2015-11-04 12:13:29 +0100

    Data provider implementation : code cleaning

--- src/app/code/community/Smile/ElasticSearch/Model/Resource/Engine/Elasticsearch/Mapping/DataProvider/Abstract.php
+++ src/app/code/community/Smile/ElasticSearch/Model/Resource/Engine/Elasticsearch/Mapping/DataProvider/Abstract.php
@@ -1,18 +1,56 @@
 <?php
-
+/**
+ * Abstract data provider : this class is meant to be used as an external (or internal) data provider for search index
+ * This permits to update search index with external data (such as ratings, page view, etc ...) coming from elsewhere
+ *
+ * This class currently supports :
+ * - differential index
+ * - full index
+ * - "partial full" index : full index of the fields added by this provider only
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile Searchandising Suite to newer
+ * versions in the future.
+ *
+ * This work is a fork of Johann Reinke <johann@bubblecode.net> previous module
+ * available at https://github.com/jreinke/magento-elasticsearch
+ *
+ * @category  Smile
+ * @package   Smile_ElasticSearch
+ * @author    Aurelien FOUCRET <aurelien.foucret@smile.fr>
+ * @copyright 2015 Smile
+ * @license   Apache License Version 2.0
+ */
 abstract class Smile_ElasticSearch_Model_Resource_Engine_Elasticsearch_Mapping_DataProvider_Abstract
 {
     /**
-     *
-     * @var Smile_ElasticSearch_Model_Resource_Engine_Elasticsearch_Mapping_Abstract
+     * @var Smile_ElasticSearch_Model_Resource_Engine_Elasticsearch_Mapping_Abstract $mapping The mapping
      */
     private $_mapping;
 
+    /**
+     * Set the mapping of the data provider
+     *
+     * @param Smile_ElasticSearch_Model_Resource_Engine_Elasticsearch_Mapping_Abstract $mapping The mapping
+     *
+     * @return Smile_ElasticSearch_Model_Resource_Engine_Elasticsearch_Mapping_DataProvider_Abstract self reference
+     */
     public function setMapping($mapping)
     {
         $this->_mapping = $mapping;
+
+        return $this;
     }
 
+    /**
+     * Run the index update, for dedicated entities and store or for the whole catalog
+     *
+     * @param int|null   $storeId   The store Id to process index for
+     * @param array|null $entityIds The entity ids being reindexed
+     *
+     * @return $this
+     */
     public function updateAllData($storeId = null, $entityIds = null)
     {
         if ($storeId == null) {
@@ -29,17 +67,35 @@ abstract class Smile_ElasticSearch_Model_Resource_Engine_Elasticsearch_Mapping_D
         return $this;
     }
 
+    /**
+     * Retrieve current index
+     *
+     * @return Smile_ElasticSearch_Model_Resource_Engine_Elasticsearch_Index
+     */
     public function getCurrentIndex()
     {
         return $this->_mapping->getCurrentIndex();
     }
 
+    /**
+     * Retrieve Elastic search client
+     *
+     * @return \Elasticsearch\Client
+     */
     public function getClient()
     {
         return $this->getCurrentIndex()->getClient();
     }
 
-    protected function _updateStoreEntities($storeId) {
+    /**
+     * Update all entities of a given store Id
+     *
+     * @param int $storeId The store Id
+     *
+     * @return void Nothing
+     */
+    protected function _updateStoreEntities($storeId)
+    {
         $bulkSize = Smile_ElasticSearch_Model_Resource_Engine_Elasticsearch_Index::COPY_DATA_BULK_SIZE;
         $scrollQuery = array(
             'index' => $this->_mapping->getCurrentIndex()->getCurrentName(),
@@ -69,13 +125,22 @@ abstract class Smile_ElasticSearch_Model_Resource_Engine_Elasticsearch_Mapping_D
         }
     }
 
+    /**
+     * Update a given list of entity for a store
+     * This method only update the fields concerned by this data provider
+     *
+     * @param int   $storeId   The store Id
+     * @param array $entityIds The entity ids
+     *
+     * @return $this
+     */
     protected function _updateEntities($storeId, $entityIds)
     {
         $updateData = $this->getEntitiesData($storeId, $entityIds);
         $bulk = array();
         foreach ($updateData as $entityId => $data) {
             $documentId = sprintf("%s|%s", $entityId, $storeId);
-            $update = $this->getCurrentIndex()->updateDocument($documentId, $data ,$this->_mapping->getType());
+            $update = $this->getCurrentIndex()->updateDocument($documentId, $data, $this->_mapping->getType());
             $bulk = array_merge($bulk, $update);
         }
 
@@ -84,5 +149,14 @@ abstract class Smile_ElasticSearch_Model_Resource_Engine_Elasticsearch_Mapping_D
         return $this;
     }
 
+    /**
+     * This method should be implemetend by descendents
+     * and will return the specific data related to this data provider for the given products/store
+     *
+     * @param int   $storeId   The store id
+     * @param array $entityIds The list of entity Ids
+     *
+     * @return mixed
+     */
     abstract public function getEntitiesData($storeId, $entityIds);
 }
