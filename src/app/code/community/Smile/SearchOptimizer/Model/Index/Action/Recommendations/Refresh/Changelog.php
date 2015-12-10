90c8b54e50003f3b2522806e7989fe6139c5aa59
Romain Ruaud <romain.ruaud@smile.fr>
2015-12-10 15:09:50 +0100

    Recommendations indexer : added a dummy changelog indexer to ensure standard magento process will not be broken

---
+++ src/app/code/community/Smile/SearchOptimizer/Model/Index/Action/Recommendations/Refresh/Changelog.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Recommendations refresh indexer.
+ * Dummy implementation for changelog, to ensure not breaking Enterprise_Index_Model_Observer::refreshIndex()
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile Searchandising Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile_SearchOptimizer
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2015 Smile
+ * @license   Apache License Version 2.0
+ */
+class Smile_SearchOptimizer_Model_Index_Action_Recommendations_Refresh_Changelog
+    extends Enterprise_Mview_Model_Action_Mview_Refresh_Changelog
+{
+    /**
+     * Refresh rows by ids from changelog.
+     *
+     * @return Smile_SearchOptimizer_Model_Index_Action_Recommendations_Refresh_Changelog self reference
+     */
+    public function execute()
+    {
+        return $this;
+    }
+}
