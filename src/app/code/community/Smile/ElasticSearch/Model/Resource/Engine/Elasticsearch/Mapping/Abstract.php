0c39025c38713911928c07487231a2074998ca40
Romain Ruaud <romain.ruaud@smile.fr>
2016-01-05 18:10:12 +0100

    Data providers : fixing several data providers being overrided by each others while reindexing

--- src/app/code/community/Smile/ElasticSearch/Model/Resource/Engine/Elasticsearch/Mapping/Abstract.php
+++ src/app/code/community/Smile/ElasticSearch/Model/Resource/Engine/Elasticsearch/Mapping/Abstract.php
@@ -475,7 +475,7 @@ abstract class Smile_ElasticSearch_Model_Resource_Engine_Elasticsearch_Mapping_A
         );
 
         foreach ($this->getDataProviders() as $dataProvider) {
-            $mapping = array_merge(
+            $mapping = array_merge_recursive(
                 $mapping,
                 $dataProvider->getMappingProperties()
             );
