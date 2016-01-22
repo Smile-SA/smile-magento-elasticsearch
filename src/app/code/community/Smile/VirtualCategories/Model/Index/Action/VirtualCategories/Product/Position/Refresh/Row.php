3d2cb9dbe632cd2a2cfb1289fb9e7618841b3858
Romain Ruaud <romain.ruaud@smile.fr>
2016-01-22 16:23:14 +0100

    Products position in virtual categories : added configurable live reindexing

---
+++ src/app/code/community/Smile/VirtualCategories/Model/Index/Action/VirtualCategories/Product/Position/Refresh/Row.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Indexer for custom products positions in Virtual categories, based on Mview integration (EE)
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Smile Searchandising Suite to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile_VirtualCategories
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2015 Smile
+ * @license   Apache License Version 2.0
+ */
+class Smile_VirtualCategories_Model_Index_Action_VirtualCategories_Product_Position_Refresh_Row
+    extends Enterprise_Mview_Model_Action_Mview_Refresh_Row
+{
+    /**
+     * Connection instance
+     *
+     * @var Varien_Db_Adapter_Interface
+     */
+    protected $_connection;
+
+    /**
+     * Mview metadata instance
+     *
+     * @var Enterprise_Mview_Model_Metadata
+     */
+    protected $_metadata;
+
+    /**
+     * Mview factory instance
+     *
+     * @var Enterprise_Mview_Model_Factory
+     */
+    protected $_factory;
+
+    /**
+     * Array of product IDs to reindex
+     *
+     * @var array
+     */
+    protected $_productIds = array();
+
+    /**
+     * Constructor with parameters
+     *
+     * @param array $args Array of arguments with keys
+     *  - 'metadata' Enterprise_Mview_Model_Metadata
+     *  - 'connection' Varien_Db_Adapter_Interface
+     *  - 'factory' Enterprise_Mview_Model_Factory
+     */
+    public function __construct(array $args)
+    {
+        $this->_app      = !empty($args['app']) ? $args['app'] : Mage::app();
+        $this->_factory  = $args['factory'];
+
+        parent::__construct($args);
+    }
+
+    /**
+     * Refresh rows by ids from changelog.
+     *
+     * @return Smile_ElasticSearch_Model_Index_Action_Search_Terms_Refresh_Changelog
+     *
+     * @throws Enterprise_Index_Model_Action_Exception
+     */
+    public function execute()
+    {
+        if (!$this->_metadata->isValid()) {
+            throw new Enterprise_Index_Model_Action_Exception("Can't perform operation, incomplete metadata!");
+        }
+
+        if (Mage::helper('smile_elasticsearch')->isActiveEngine() == true) {
+
+            $this->_setProductIdsFromValue();
+
+            $engine       = Mage::helper('catalogsearch')->getEngine();
+            $mapping      = $engine->getCurrentIndex()->getMapping('product');
+            $dataprovider = $mapping->getDataProvider('virtual_categories_products_position');
+
+            $dataprovider->updateAllData(null, $this->_productIds);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set value ID to product IDs to be re-indexed
+     *
+     * @return void Nothing
+     */
+    protected function _setProductIdsFromValue()
+    {
+        $this->_productIds = $this->_keyColumnIdValue;
+    }
+}
