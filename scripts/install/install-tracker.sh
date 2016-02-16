e5e1dba1dad03626f535c4a878b8ab5ba397a72a
Romain Ruaud <romain.ruaud@smile.fr>
2016-02-16 17:51:45 +0100

    Tracker module, added configurable ES Hosts for Logstash output

--- scripts/install/install-tracker.sh
+++ scripts/install/install-tracker.sh
@@ -6,13 +6,14 @@
 # Configuration stuffs
 LOGSTASH_VERSION=2.1
 
-if [ "$#" -lt 1 ]; then
-    echo "Usage : ./install-tracker.sh TRACKER_LOG_FILE"
+if [ "$#" -lt 2 ]; then
+    echo "Usage : ./install-tracker.sh TRACKER_LOG_FILE es_server1:port <es_server_2:port> ..."
     echo "Eg. ./install-tracker.sh /var/log/smile_searchandising_suite/apache_raw_events/*.log"
     exit 1
 fi
 
 TRACKER_LOG_FILE=${1%/}
+HOSTS=`printf -- "\"%s\", " ${@:2} | rev | cut -c3- | rev`
 
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 
@@ -23,14 +24,14 @@ apt-get install logstash
 
 # Deploy Logstash configuration
 cp -rfv logstash-configuration/es-template.json /etc/logstash/
-sed -e "s/SMILE_ELASTICSUITE_TRACKER_TEMPLATE/\/etc\/logstash\/es-template.json/" logstash-configuration/injest-events-output.conf.sample > /etc/logstash/conf.d/injest-events-output.conf
+sed -e "s/SMILE_ELASTICSUITE_TRACKER_TEMPLATE/\/etc\/logstash\/es-template.json/;s/HOSTS/$HOSTS/;" logstash-configuration/injest-events-output.conf.sample > /etc/logstash/conf.d/injest-events-output.conf
 sed -e "s~SMILE_TRACKER_LOG_FILE~$TRACKER_LOG_FILE~" logstash-configuration/injest-events-input.conf.sample > /etc/logstash/conf.d/injest-events-input.conf
 cp -rfv logstash-configuration/injest-events-filter.conf.sample /etc/logstash/conf.d/injest-events-filter.conf
 
 # Ensure corrects ACL for logstash on files
 mkdir -p /var/log/smile_searchandising_suite/apache_raw_events/
 setfacl -Rm u:logstash:rX /var/log/apache2
-setfacl -m u:logstash:r /var/log/smile_searchandising_suite/apache_raw_events/
+setfacl -m u:logstash:rX /var/log/smile_searchandising_suite/apache_raw_events/
 setfacl -m u:logstash:r $TRACKER_LOG_FILE
 
 # Start Logstash and ensure it starts with the system
